6f074aee64631baa6439806c5dc580b7dde01588
safaanilatasoy <safaanilatasoy@gmail.com>
2022-12-07 18:28:54 +0300

    main

--- 10daysofJs/Day2_loops.js
+++ 10daysofJs/Day2_loops.js
@@ -1,25 +1,28 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
     });
-    
-    main();    
+
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -27,21 +30,20 @@ function readLine() {
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-   for(let i=0; i<s.length; i++){
-        if(s.charAt(i).match(/[aeiou]/)){
-        console.log(s.charAt(i));
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i).match(/[aeiou]/)) {
+      console.log(s.charAt(i));
     }
-   }
-  for(let i=0; i<s.length; i++){
-        if(s.charAt(i).match(/[^aeiou]/)){
-        console.log(s.charAt(i));
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i).match(/[^aeiou]/)) {
+      console.log(s.charAt(i));
     }
-   }
+  }
 }
 
-
 function main() {
-    const s = readLine();
-    
-    vowelsAndConsonants(s);
+  const s = readLine();
+
+  vowelsAndConsonants(s);
 }
