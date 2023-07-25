13be7a802db4988a667d2cf9988fed205eec5a94
safaanilatasoy <safaanilatasoy@gmail.com>
2023-07-25 22:19:59 +0300

    main

--- 30_Days_of_Code/Day7-Arrays.js
+++ 30_Days_of_Code/Day7-Arrays.js
@@ -1,9 +1,12 @@
 function main() {
-    const n = parseInt(readLine().trim(), 10);
-    let temp;
-    let arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    
-    temp = arr.reverse();
-    temp = temp.join(" ");
-     console.log(temp);
+  const n = parseInt(readLine().trim(), 10);
+  let temp;
+  let arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  temp = arr.reverse();
+  temp = temp.join(" ");
+  console.log(temp);
 }
